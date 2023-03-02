8595da9c817eed8baf979c1d74d8af550ad93270
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-03-02 06:24:21 +0000

    revised css grid

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,11 @@
 export function Meme() {
     return (
-        <form>
-            <input type="text" name="top" value="Shut up" />
-            <input type="text" name="bottom" value="And take my money" />
-            <button type="submit">Get a new meme image  🖼</button>
-        </form>
+        <main>
+            <form>
+                <input type="text" name="top" value="Shut up" />
+                <input type="text" name="bottom" value="And take my money" />
+                <button type="submit">Get a new meme image 🖼</button>
+            </form>
+        </main>
     )
 }
