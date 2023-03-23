5bcdb047a12560a51058f1392b8b4c815afd0f8e
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-03-23 01:49:58 +0000

    meme text on a picture

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
+import memesData from '../memesData.js'
+
 export function Meme() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prev=>({...prev, [name]: value}))
+    }
+
+    function buttonClickHandler() {
+        setMeme(prev => {
+            let data = prev.randomImage
+            while (data == prev.randomImage) data = allMemeImages.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url
+            return { ...prev, randomImage: data }
+        })
+    }
+
     return (
         <main>
-            <form>
-                <input type="text" name="top" value="Shut up" />
-                <input type="text" name="bottom" value="And take my money" />
-                <button type="submit">Get a new meme image 🖼</button>
-            </form>
+            <div className="form">
+                <input type="text" name="topText" placeholder="Shut up" onChange={handleChange} />
+                <input type="text" name="bottomText" placeholder="and take my money!" onChange={handleChange}/>
+                <button onClick={buttonClickHandler} type="submit">Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
